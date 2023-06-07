d2cf7830afceca83055158b578e460c7418b850f
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-07 16:24:07 -0300

    aplicando estilização no global css

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from "next/app";
+import { globalStyles } from "./pages/styles/global";
+
+globalStyles()
 
 export default function App({Component, pageProps} : AppProps) {
     return <Component {...pageProps} />
