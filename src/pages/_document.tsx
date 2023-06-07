a48457d4aae2a7f426cdf864eb6ddf9dada08890
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-07 16:59:55 -0300

    passando função getCssText do ignite-ui para poder ter estilização mesmo com js desabilitado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from "@ignite-ui/react";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function DOcument() {
@@ -14,6 +15,7 @@ export default function DOcument() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <style id="stiches"  dangerouslySetInnerHTML={{__html: getCssText}}/>
       </Head>
       <body>
         <Main />
