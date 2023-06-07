be0bcaf4d96293cb014e53647633eceb4e6d4d24
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-07 17:55:31 -0300

    configurando imagem da land page do usuário, colocando tag de priority e colocando a qualidade alta

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,25 @@
-import {Heading, Text} from '@ignite-ui/react'
+import { Heading, Text } from "@ignite-ui/react";
+import { Container, Hero, Preview } from "./style";
+import Image from 'next/image'
+import previewImage from '../../assets/Preview.png'
+
 export default function Home() {
   return (
-    <Heading as="h1">Hello WOrld</Heading>
-  )
+    <div>
+      <Container>
+        <Hero>
+          <Heading size={"4xl"}>
+            Agendamento descomplicado
+            <Text size={"lg"}>
+              Conecte seu calendário e permita que as pessoas marquem
+              agendamentos no seu tempo livre
+            </Text>
+          </Heading>
+        </Hero>
+        <Preview>
+          <Image src={previewImage} height={400} alt="" quality={100} priority/>
+        </Preview>
+      </Container>
+    </div>
+  );
 }
---
+++ src/pages/home/style.ts
@@ -0,0 +1,9 @@
+import {styled} from '@ignite-ui/react'
+
+export const Container = styled('div', {
+
+})
+
+export const Hero = styled('div', {})
+
+export const Preview = styled('div', {})
