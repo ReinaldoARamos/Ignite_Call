12d93052e363e4aa62f5a1b944f476a9da1f48ef
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-12 14:49:19 -0300

    passnaod um setValue com o username vindo do parametro

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -4,30 +4,44 @@ import { ArrowRight } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const registerFormSchema = z.object({
-  userName: z
-  .string()
-  .min(3, {message : 'O usuário precisa ter pelo menos 3 letras'})
-  .regex(/^([a-z\\-]+)$/i, {message : 'O usuário pode ter apenas letras e hifens'})
-  .transform((username) => username.toLowerCase()),
+  username: z
+    .string()
+    .min(3, { message: "O usuário precisa ter pelo menos 3 letras" })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: "O usuário pode ter apenas letras e hifens",
+    })
+    .transform((username) => username.toLowerCase()),
 
-  name: z.string().min(3, {message : 'O nome precisa ter pelo menos 3 letras'})
-})
+  name: z
+    .string()
+    .min(3, { message: "O nome precisa ter pelo menos 3 letras" }),
+});
 
-type RegisterFormData = z.infer<typeof registerFormSchema>
+type RegisterFormData = z.infer<typeof registerFormSchema>;
 export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
-    resolver: zodResolver(registerFormSchema)
+    resolver: zodResolver(registerFormSchema),
   });
 
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', router.query.username);
+    }
+  }, [router.query?.username, setValue]);
+
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    console.log(data);
   }
   return (
     <Container>
@@ -42,15 +56,23 @@ export default function Register() {
       <Form as="form" onSubmit={handleSubmit(handleRegister)}>
         <label>
           <Text size="sm">Usuário</Text>
-          <TextInput prefix="ignite.com/" placeholder="seu-usuário" {...register('userName')}/>
-          {errors.userName && (
-            <FormError size="sm">{errors.userName.message}</FormError>
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="seu-usuário"
+            {...register("username")}
+          />
+          {errors.username && (
+            <FormError size="sm">{errors.username.message}</FormError>
           )}
         </label>
 
         <label>
           <Text size="sm">Nome Completo</Text>
-          <TextInput prefix="ignite.com/" placeholder="seu-nome"  {...register('name')}/>
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="seu-nome"
+            {...register("name")}
+          />
           {errors.name && (
             <FormError size="sm">{errors.name.message}</FormError>
           )}
