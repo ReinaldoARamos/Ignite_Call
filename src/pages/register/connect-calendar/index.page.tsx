1f99b2fe6edf2700528cd1968fcffc9ba2fdf9a9
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-14 10:00:55 -0300

    usando o useSession para colocar um valor dentro de uma variável que verifica o status de authorized, caso esteja unauthenticated ele deixa normal, caso esteja authenticated ele desabilita o conectar e habilita o botão de próximo passo

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,10 +1,17 @@
 import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
 
 import { ArrowRight } from "lucide-react";
-import {signIn, useSession} from 'next-auth/react'
+import { signIn, useSession } from "next-auth/react";
 import { api } from "@/lib/axios";
-import { AuthError, ConnectBox, ConnectItem, Container, Header } from "./styles";
+import {
+  AuthError,
+  ConnectBox,
+  ConnectItem,
+  Container,
+  Header,
+} from "./styles";
 import { useRouter } from "next/router";
+import { Checks } from "phosphor-react";
 /*
 
 
@@ -25,46 +32,50 @@ import { useRouter } from "next/router";
   }
 * */
 
-
 export default function connectCalender() {
+  const session = useSession();
+  const router = useRouter();
 
-  const session = useSession()
-  const router = useRouter()
+  const hasAuthError = !!router.query.error;
 
-  const hasAuthError = !!router.query.error
-
-
+  const isSignedIn = session.status === "authenticated";
   return (
     <Container>
       <Header>
         <Heading as="strong" />
-        <Text>
-          Conecte sua agenda
-        </Text>
+        <Text>Conecte sua agenda</Text>
         <MultiStep size={4} currentStep={2}></MultiStep>
       </Header>
       <ConnectBox>
         <ConnectItem>
-            <Text>
-                Google Calendar
-            </Text>
-            <Button variant='secondary' size="sm" onClick={() => signIn('google')}>
-          Conectar
-          <ArrowRight />
-        </Button>
+          <Text>Google Calendar</Text>
+          {isSignedIn ? (
+            <Button size='sm' disabled >
+              Conectado
+              <Checks />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={() => signIn("google")}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
         </ConnectItem>
-       {hasAuthError && (
-        <AuthError size='sm'>
-            Falha ao se conectar ao Google, verifique se você habilitou as opções de acesso ao Google Calander
-        </AuthError>
-       )}
-        <Button >
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            opções de acesso ao Google Calander
+          </AuthError>
+        )}
+        <Button disabled={!isSignedIn}>
           Próximo passo
           <ArrowRight />
         </Button>
       </ConnectBox>
-     
     </Container>
-    
   );
 }
