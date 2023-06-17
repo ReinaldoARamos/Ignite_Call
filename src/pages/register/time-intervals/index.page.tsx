0933368f350907c76e83abe23592765abe105433
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-17 16:59:32 -0300

    importando zod  e react use form

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,4 +1,11 @@
-import { Button, Checkbox, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from "@ignite-ui/react";
 import { Container, Header } from "../styles";
 import {
   IntervalBox,
@@ -8,8 +15,21 @@ import {
   IntervalItem,
 } from "./style";
 import { ArrowRight, Check } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import z from 'zod';
 
+const TimeIntervalsFormSchema = z.object({
+    
+})
 export default function connectCalender() {
+    const { register, handleSubmit, formState: {
+        isSubmitting,
+        errors
+    }} = useForm()
+
+async function handleSetTimeIntervals() {
+    
+}
   return (
     <Container>
       <Header>
@@ -18,43 +38,33 @@ export default function connectCalender() {
         <MultiStep size={4} currentStep={3}></MultiStep>
       </Header>
 
-      <IntervalBox as="form">
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
         <IntervalContainer>
           <IntervalItem>
             <IntervalDay>
-                <Checkbox  />  
-                <Text>
-                    Segunda-feira
-                </Text>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
             </IntervalDay>
             <IntervalInputs>
-              <TextInput size="sm" type="time" step={60}/>
-              <TextInput size="sm" type="time" step={60}/>
-              
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
             </IntervalInputs>
-            
-
-            
           </IntervalItem>
 
           <IntervalItem>
             <IntervalDay>
-            <Checkbox  />  
-                <Text>
-                    Terça-feira
-                </Text>
+              <Checkbox />
+              <Text>Terça-feira</Text>
             </IntervalDay>
             <IntervalInputs>
-              <TextInput size="sm" type="time" step={60}/>
-              <TextInput size="sm" type="time" step={60}/>
-              
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
             </IntervalInputs>
-            
-
-            
           </IntervalItem>
         </IntervalContainer>
-        <Button type="submit">Próximo passo <ArrowRight /></Button>
+        <Button type="submit">
+          Próximo passo <ArrowRight />
+        </Button>
       </IntervalBox>
     </Container>
   );
