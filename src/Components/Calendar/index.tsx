ff3ec3ebcadbf713fa4ad443669c48be588eac14
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-21 17:10:02 -0300

    criando a estrutura do header do calendário

--- src/Components/Calendar/index.tsx
+++ src/Components/Calendar/index.tsx
@@ -1,4 +1,42 @@
-export function Calender(){
-    
-    
+import { CaretLeft, CaretRight } from "phosphor-react";
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarHeader,
+  CalendarTitle,
+} from "./style";
+import { getWeekDays } from "@/utils/get-weekdays";
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true });
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Dezembro <span>2022</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+           {shortWeekDays.map((weekday) => {
+            return <th key={weekday}>{weekday}.</th>
+           })}
+          </tr>
+        </thead>
+      </CalendarBody>
+    </CalendarContainer>
+  );
 }
--- src/Components/Calendar/style.ts
+++ src/Components/Calendar/style.ts
@@ -1,13 +1,13 @@
 import { Text, styled } from "@ignite-ui/react";
 
-export const CalenderContainer = styled("div", {});
+export const CalendarContainer = styled("div", {});
 
-export const CalenderHeader = styled("div", {});
+export const CalendarHeader = styled("div", {});
 
-export const CalenderTitle = styled(Text, {});
+export const CalendarTitle = styled(Text, {});
 
-export const CalenderActions = styled("div", {});
+export const CalendarActions = styled("div", {});
 
-export const CalenderBody = styled("table", {});
+export const CalendarBody = styled("table", {});
 
-export const CalenderDay = styled("button", {});
+export const CalendarDay = styled("button", {});
