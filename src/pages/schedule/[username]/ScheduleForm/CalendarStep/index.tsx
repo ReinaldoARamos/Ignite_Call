9274beed29271a3f8a6f20de2cc4564975bd31bc
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-22 13:42:07 -0300

    criando a estilização da lista de tempo

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,16 +1,36 @@
 import { Calendar } from "@/Components/Calendar";
-import { Container, TimePicker } from "./styles";
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from "./styles";
 
 export function CalendarStep() {
-
-    const isDateSelected = true
+  const isDateSelected = true;
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
 
       {isDateSelected && (
-        <TimePicker></TimePicker>
+        <TimePicker>
+          <TimePickerHeader>
+            Terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
 
+          <TimePickerList>
+            <TimePickerItem>08:00hr</TimePickerItem>
+            <TimePickerItem>08:00hr</TimePickerItem>
+            <TimePickerItem>08:00hr</TimePickerItem>
+            <TimePickerItem>08:00hr</TimePickerItem>
+            <TimePickerItem>08:00hr</TimePickerItem>
+            <TimePickerItem>08:00hr</TimePickerItem>
+            <TimePickerItem>08:00hr</TimePickerItem>
+            <TimePickerItem>08:00hr</TimePickerItem>
+            <TimePickerItem>08:00hr</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
       )}
     </Container>
   );
