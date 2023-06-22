de56b1dc69f31d916054e3f7eabc176c0e2e06e7
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-22 10:34:30 -0300

    Adicionando a parte visual do calendário

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,10 @@
-
 import { Calendar } from "@/Components/Calendar";
 import { Container } from "./styles";
 
 export function CalendarStep() {
-    return (
-    <Container >
-<Calendar/>
-    </Container>)
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  );
 }
