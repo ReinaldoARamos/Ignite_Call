1cfb4769139e2b1020f03105e53fef41b0c12bf5
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-22 15:02:18 -0300

    estilizando o formulário

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,41 @@
+import { CalendarBlank } from "phosphor-react";
+import { ConfirmForm, FormActions, FormHeader } from "./styles";
+import { Text, TextArea, TextInput, Button } from "@ignite-ui/react";
+import { Clock } from "lucide-react";
+
 export function ConfirmStep() {
-    
+  function handleConfirmScheduling() {}
+  return (
+    <ConfirmForm as={"form"} onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro
+        </Text>
+
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+      <label>
+        <Text size="sm">Nome Completo</Text>
+        <TextInput placeholder="Seu Nome"/>
+      </label>
+      <label>
+        <Text size="sm">Endereço de E-mail</Text>
+        <TextInput placeholder="Seu E-Mail"/>
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de E-mail</Text>
+        <TextArea />
+      </label>
+    <FormActions>
+        <Button type="button" variant={"tertiary"}>Cancelar</Button>
+        <Button type="submit">Confirmar</Button>
+    </FormActions>
+
+    </ConfirmForm>
+  );
 }
