128bd053881cf232c3b9aa503cfb8dd4a0a98dea
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-23 11:13:44 -0300

    fazendo o cálculo para retornar os valores do final do mes passado e o começo do mês seguinte e armazenar todos dentro de um array

--- src/Components/Calendar/index.tsx
+++ src/Components/Calendar/index.tsx
@@ -19,41 +19,86 @@ export function Calendar() {
   function handlePreviousMonth() {
     const previousMonth = currentDate.subtract(1, "month");
 
-    setCurrentDate(previousMonth)
+    setCurrentDate(previousMonth);
   }
 
   function handleNextMonth() {
     const NextMonth = currentDate.add(1, "month");
 
-    setCurrentDate(NextMonth)
+    setCurrentDate(NextMonth);
   }
 
   const currentYear = currentDate.format("YYYY");
   const currentMonth = currentDate.format("MMMM");
 
+
+  //-----------------------------------------------------------------
   const calendarWeeks = useMemo(() => {
-    const daysInMonthArray = Array.from({ //constróu um array
-      length: currentDate.daysInMonth() //pega o currentDate e o daysinmonth da api dayjs
-    }).map((_, i) => { //faz um map retornando o index, ja que ovalor de todos é undefined
-      return currentDate.set('date', i + 1)  //pega o valor e faz um set date(DIA do mes ) i(index) + 1 
-    })
-
-    const firtsWeekDay = currentDate.get('day')
-    
-    const previousMonthFillArray = Array.from({
-      length: firtsWeekDay
+    const daysInMonthArray = Array.from({
+      //constróu um array
+      length: currentDate.daysInMonth(), //pega o currentDate e o daysinmonth da api dayjs
     }).map((_, i) => {
-      return currentDate.subtract(i + 1, 'day')
-    }).reverse()
-  
-    return [
-      ...previousMonthFillArray,
-      ...daysInMonthArray
+      //faz um map retornando o index, ja que ovalor de todos é undefined
+      return currentDate.set("date", i + 1); //pega o valor e faz um set date(DIA do mes ) i(index) + 1
+    });
+
+
+    const lastWeekDay = currentDate
+      .set("date", currentDate.daysInMonth()) //pega o último dia do mes
+      .get("day");
+
+    //------------------------------------------
+
+    const lastDayInCurrentMonth = currentDate.set(
+      "date",
+      currentDate.daysInMonth()
+    ); //pega o último dia do mes
+
+       
+    const firtsWeekDay = lastDayInCurrentMonth.get("day");
+   //------------------------------------------
+ 
+    //------------------------------------------
+    const previousMonthFillArray = Array.from({
+      length: firtsWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, "day");
+      })
+      .reverse();
+
+    //------------------------------------------
+
+    //------------------------------------------
+
+    const nextMonthFillArray = Array.from({
+      length: 7 - (lastWeekDay + 1),
+    }).map((_, i) => {
+      return lastDayInCurrentMonth.add(i + 1, "day");
+    });
+
+    const calendarDays = [
+      ...previousMonthFillArray.map(date => {
+        return {date, disabled: true}
+      }),
+      ...nextMonthFillArray.map(date => {
+        return {date, disabled: true}
+      }),,
+      ...daysInMonthArray.map(date => {
+        return {date, disabled: false}
+      }),
     ]
-  }, [currentDate])
+  
+ const calendarWeeks = calendarDays.reduce(() => {
 
+ } , [])
+ 
+    return calendarDays
+  }, [currentDate]);
 
-  console.log(calendarWeeks)
+  console.log(calendarWeeks);
+
+  //------------------------------------------
   return (
     <CalendarContainer>
       <CalendarHeader>
@@ -68,7 +113,7 @@ export function Calendar() {
           </button>
 
           <button>
-            <CaretRight  onClick={handleNextMonth} />
+            <CaretRight onClick={handleNextMonth} />
             <title>Next Month</title>
           </button>
         </CalendarActions>
