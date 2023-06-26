9d7b99e08b656b925198adab2f07bd27060e8614
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-26 14:08:40 -0300

    retornando um MAP com os horários disponíveis porém os horários availables nao estão funcionando e ficando desabiltiados

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -11,36 +11,41 @@ import dayjs from "dayjs";
 import { api } from "@/lib/axios";
 import { useRouter } from "next/router";
 
+interface Availability {
+  possibleTimes: number[]
+  availableTimes: number[]
+}
+
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [availability, setAvailability] = useState(null);
+  const [availability, setAvailability] = useState<Availability | null>(null);
   const isDateSelected = !!selectedDate; //condicional, mostra o time picker quando a data é selecionada
 
   const weekDay = selectedDate ? dayjs(selectedDate).format("dddd") : null; //data selecionada formatada
- const router = useRouter() //router para pegar o username na query
- const username = String(router.query.username) // username da query convertido em string
+  const router = useRouter(); //router para pegar o username na query
+  const username = String(router.query.username); // username da query convertido em string
 
   const describeDate = selectedDate
     ? dayjs(selectedDate).format("DD[ de ] MMMM") //mes formatado
     : null;
 
   useEffect(() => {
-
-    if(!selectedDate){
-      return
+    if (!selectedDate) {
+      return;
     }
 
-    api.get(`users/${username}/availability`,{
-      params: {
-        date: dayjs(selectedDate).format('YYYY-MM-DD')
-      }
-    }).then(response => {
-      console.log(response.data)
-    })
+    api
+      .get(`users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format("YYYY-MM-DD"),
+        },
+      })
+      .then((response) => {
+        setAvailability(response.data);
+      });
   }, [selectedDate, username]);
-   //quando a selectedDate é alterada, ele faz uma chamada
+  //quando a selectedDate é alterada, ele faz uma chamada
   //na API e retorna os possible times e available times
-
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
@@ -53,15 +58,16 @@ export function CalendarStep() {
           </TimePickerHeader>
 
           <TimePickerList>
-            <TimePickerItem>08:00hr</TimePickerItem>
-            <TimePickerItem>08:00hr</TimePickerItem>
-            <TimePickerItem>08:00hr</TimePickerItem>
-            <TimePickerItem>08:00hr</TimePickerItem>
-            <TimePickerItem>08:00hr</TimePickerItem>
-            <TimePickerItem>08:00hr</TimePickerItem>
-            <TimePickerItem>08:00hr</TimePickerItem>
-            <TimePickerItem>08:00hr</TimePickerItem>
-            <TimePickerItem>08:00hr</TimePickerItem>
+            {availability?.possibleTimes.map((hour) => {
+              return (
+                <TimePickerItem
+                  key={hour}
+                  disabled={!availability?.availableTimes.includes(hour)}
+                >
+                  {String(hour).padStart(2, "0")}:00
+                </TimePickerItem>
+              );
+            })}
           </TimePickerList>
         </TimePicker>
       )}
