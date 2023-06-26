125cc60ec2f32bfb78d684b683c85844bf7cd2c2
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-26 15:31:31 -0300

    instalando e configurando o react-query, colocando ele por volta do App com o provider

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,6 +2,7 @@ import '../lib/dayjs'
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { globalStyles } from "./styles/global";
+import { QueryClientProvider } from '@tanstack/react-query';
 
 globalStyles();
 
@@ -10,8 +11,10 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
+    <QueryClientProvider>
     <SessionProvider session={session}> 
       <Component {...pageProps} />
     </SessionProvider>
+    </QueryClientProvider>
   );
 }
