3b02150553f9784475ed1d91c5b2a67ce3352e52
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-30 10:48:14 -0300

    criando a rota da api para criação dos schedulings, fazendoa a validação dos campos usando zod

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../../lib/prisma";
+import { z } from "zod";
 import dayjs from "dayjs";
-
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
@@ -11,11 +11,6 @@ export default async function handle(
   }
 
   const username = String(req.query.username); //pega o username da query da URL
-  const { date } = req.query; //pega na query da data a data selecionada
-
-  if (!date) {
-    return res.status(400).json({ message: "Date not Valid" });
-  }
 
   const user = await prisma.user.findUnique({
     where: {
@@ -27,5 +22,47 @@ export default async function handle(
     return res.status(400).json({ message: "User  not Found" });
   }
 
+  const createSchedulingBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    observation: z.string(),
+    date: z.string().datetime(),
+  });
+  const { name, email, observation, date } = createSchedulingBodySchema.parse(
+    req.body
+  );
+
+
+  const schedulingDate = dayjs(date).startOf('hour')
+
+  if (!date) {
+    return res.status(400).json({ message: "Date not Valid" });
+  }
+
+  if (schedulingDate.isBefore(new Date())) {
+    return res.status(400).json({ message: "Date is in the past" });
+  }
+
+  const conflitScheduling = await prisma.scheduling.findFirst({
+    where: {
+      user_id: user.id,
+      date: schedulingDate.toDate()
+    }
+  })
+
+
+  if (conflitScheduling) {
+    return res.status(400).json({ message: "There is another scheduling at the same time" });
+  }
+
+  await prisma.scheduling.create({
+    data: {
+      date: schedulingDate.toDate(),
+      name,
+      observation,
+      email,
+      user_id: user.id
+    }
+  })
   return res.json({});
 }
