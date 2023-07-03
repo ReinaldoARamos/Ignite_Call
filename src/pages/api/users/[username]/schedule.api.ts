afa180423fdf0e029f11738642e6990664a350ac
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-07-03 15:41:40 -0300

    Criando o evento do ignitecall com a API do google, funcionando, porém ao confirmar da erro 400(invalid request)

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -2,6 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../../lib/prisma";
 import { z } from "zod";
 import dayjs from "dayjs";
+import { google } from "googleapis";
+import { getGoogleOAuthToken } from "@/lib/google";
+import { randomUUID } from "crypto";
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
@@ -32,8 +35,7 @@ export default async function handle(
     req.body
   );
 
-
-  const schedulingDate = dayjs(date).startOf('hour')
+  const schedulingDate = dayjs(date).startOf("hour");
 
   if (!date) {
     return res.status(400).json({ message: "Date not Valid" });
@@ -46,23 +48,60 @@ export default async function handle(
   const conflitScheduling = await prisma.scheduling.findFirst({
     where: {
       user_id: user.id,
-      date: schedulingDate.toDate()
-    }
-  })
-
+      date: schedulingDate.toDate(),
+    },
+  });
 
   if (conflitScheduling) {
-    return res.status(400).json({ message: "There is another scheduling at the same time" });
+    return res
+      .status(400)
+      .json({ message: "There is another scheduling at the same time" });
   }
 
-  await prisma.scheduling.create({
+ const scheduling =  await prisma.scheduling.create({
     data: {
       date: schedulingDate.toDate(),
       name,
       observation,
       email,
-      user_id: user.id
-    }
-  })
+      user_id: user.id,
+    },
+  });
+
+  const calendar = google.calendar({
+    version: "v3",
+    auth: await getGoogleOAuthToken(user.id),
+  });
+
+  await calendar.events.insert({
+    calendarId: "primary", //pega o caldendario default
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observation,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, "hour").format(),
+      },
+      attendees: [
+        {
+          email,
+          displayName: name,
+        },
+      ],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+
+          }
+        }
+      }
+    },
+  });
+
   return res.status(201).end();
 }
