9c8fa130f3c0a545caa3eb93f0b46a13c91214b7
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-07-04 17:21:58 -0300

    testando express

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: '/api'
+    baseURL: '/api',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Acess-Control-Alow-Origin': '*'
+    }
 })
