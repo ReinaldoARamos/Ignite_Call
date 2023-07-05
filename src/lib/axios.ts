d351551cd63192ae3d74f8fc72897c747ebb6ad9
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-07-05 11:24:58 -0300

    subindo nova chave DB

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,8 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: '/api',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Acess-Control-Alow-Origin': '*'
-    }
+ 
 })
